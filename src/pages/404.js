8910c77224c4a5ef49f337f85224b1efc5986aae
DenDense <denyfarras@gmail.com>
2020-12-18 09:01:34 +0700

    Adding 404 Page & Search Page File

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,24 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import SEO from "../components/Seo"
 
 const notfound = () => (
-  <div>
+  <div className="d-flex align-items-center mt-3">
     <SEO title="Page Not Found" />
-    <div className="container">Page Not Found</div>
+    <div className="container d-flex flex-column">
+      <div className="d-flex justify-content-center">
+        <img src="https://i.ibb.co/yVr4pnk/5.png" alt="404 Not Found" />
+      </div>
+      <div className="d-flex justify-content-center">
+        <h2>This Page is Lost in Space</h2>
+      </div>
+      <div className="d-flex justify-content-center">
+        <Link to="/" className="btn btn-primary">
+          Back to Home
+        </Link>
+      </div>
+    </div>
   </div>
 )
 
