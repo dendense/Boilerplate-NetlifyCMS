1a3a8d157f5d7a499b810af612d7b72dddea1ee4
jpngeekudin <udinshalah12@gmail.com>
2022-06-21 12:14:30 +0700

    fix theme switch

--- src/components/ThemeSwitch.js
+++ src/components/ThemeSwitch.js
@@ -1,22 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Dropdown } from "react-bootstrap";
 import { THEME_LIST } from "../configs/themeConfig";
 import { titleCase } from "../utils/utilityFunction";
 
 export default function ThemeSwitch() {
-  let currentTheme =
-    document.body.className.replace("theme-", "") ||
-    localStorage.getItem("theme");
+  const [activeTheme, setActiveTheme] = useState('default');
 
-  if (currentTheme && THEME_LIST.includes(currentTheme)) {
-    document.body.className = `theme-${currentTheme}`;
-  } else {
-    currentTheme = THEME_LIST[0];
-    document.body.className = `theme-${currentTheme}`;
-    localStorage.setItem("theme", currentTheme);
-  }
-
-  const [activeTheme, setActiveTheme] = useState(currentTheme);
+  useEffect(() => {
+    let currentTheme =
+      document.body.className.replace("theme-", "") ||
+      localStorage.getItem("theme");
+  
+    if (currentTheme && THEME_LIST.includes(currentTheme)) {
+      document.body.className = `theme-${currentTheme}`;
+    } else {
+      currentTheme = THEME_LIST[0];
+      document.body.className = `theme-${currentTheme}`;
+      localStorage.setItem("theme", currentTheme);
+    }
+  }, []);
 
   const switchTheme = (theme) => {
     if (THEME_LIST.includes(theme)) {
